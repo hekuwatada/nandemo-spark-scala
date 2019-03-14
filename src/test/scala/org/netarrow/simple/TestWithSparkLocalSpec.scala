d4bb7091908e6183f1f7e19d003c5444f441920a
Heku Watada <heku.watada@gmail.com>
2019-03-14 11:20:59 +0000

    Added example of creating RDD with Spark local.

--- src/test/scala/org/netarrow/simple/TestWithSparkLocalSpec.scala
+++ src/test/scala/org/netarrow/simple/TestWithSparkLocalSpec.scala
@@ -1,6 +1,8 @@
 package org.netarrow.simple
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.{FunSpec, Matchers}
 
 /**
@@ -11,16 +13,16 @@ class TestWithSparkLocalSpec extends FunSpec with Matchers {
 
   private def createSparkSession(appName: String): SparkSession = {
     SparkSession.builder()
-        .appName(appName)
-        //NOTE: Local in-process mode
-        // driver is used for execution
-        // uses as many threads as the number of processors available to JVM
-        //@see https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-local.html
-        .master("local[*]")
-        .getOrCreate()
+      .appName(appName)
+      //NOTE: Local in-process mode
+      // driver is used for execution
+      // uses as many threads as the number of processors available to JVM
+      //@see https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-local.html
+      .master("local[*]")
+      .getOrCreate()
   }
 
-  def withSparkLocal(testBlock: (SparkSession) => Unit): Unit = {
+  private def withSparkSession(testBlock: (SparkSession) => Unit): Unit = {
     val ss = createSparkSession(appName)
     try {
       testBlock(ss)
@@ -28,10 +30,26 @@ class TestWithSparkLocalSpec extends FunSpec with Matchers {
       ss.sparkContext.stop()
     }
   }
-  
+
+  private def withSparkContext(testBlock: (SparkContext) => Unit): Unit =
+    withSparkSession((ss: SparkSession) => testBlock(ss.sparkContext))
+
+
   describe("Testing with local Spark") {
-    it("starts a local Spark") { withSparkLocal { ss: SparkSession =>
+    it("starts a local Spark") {
+      withSparkSession { ss: SparkSession =>
         ss.sparkContext.isLocal shouldBe true
+      }
+    }
+  }
+
+  describe("RDD") {
+    it("creates RDD") {
+      withSparkContext { sc: SparkContext =>
+        val numbers: Seq[Int] = Range(1, 1000000)
+        val rdd: RDD[Int] = sc.parallelize(numbers)
+        val filteredRdd: RDD[String] = rdd.filter(_ % 2 != 0).map(_.toString)
+        filteredRdd.count() shouldBe 500000
       }
     }
   }
