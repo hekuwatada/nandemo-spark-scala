25dc11912fb6b94e111042bc0e402840f379ba82
Heku Watada <heku.watada@gmail.com>
2019-05-23 02:18:46 +0100

    Added example of write to / read from a local MongoDB server using Mongo connector.

--- build.sbt
+++ build.sbt
@@ -7,13 +7,14 @@ version := "0.1"
 // Recommended to use Scala 2.11.x and SBT 0.13.x
 scalaVersion := "2.11.12"
 
-val sparkVersion = "2.2.0"
+val sparkVersion = "2.4.0"
 val typesafeVersion = "1.3.4"
 
 val prodLibs = Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion, // required for SparkSession
-  "com.typesafe" % "config" % typesafeVersion
+  "com.typesafe" % "config" % typesafeVersion,
+  "org.mongodb.spark" %% "mongo-spark-connector" % sparkVersion
 )
 
 val scalaTestVersion = "3.0.5"
---
+++ src/main/scala/org/netarrow/mongodb/ConnectToMongoDb.scala
@@ -0,0 +1,38 @@
+package org.netarrow.mongodb
+
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.netarrow.model.User
+
+object ConnectToMongoDb extends App {
+
+  //@see https://docs.mongodb.com/spark-connector/master/scala-api/
+  /* Create the SparkSession.
+ * If config arguments are passed from the command line using --conf,
+ * parse args for the values to set.
+ */
+
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession.builder()
+    .master("local")
+    .appName("MongoSparkConnectorIntro")
+    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test2.user")
+    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test2.user")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val users: Dataset[User] = spark.sparkContext
+    .parallelize(Seq(User("bear", 10), User("rabbit", 5)))
+    .toDS()
+
+  MongoSpark.save(users)
+
+  val df: DataFrame = MongoSpark.load(spark)
+  val ret = df.count()
+
+  println(ret)
+
+  spark.stop()
+}
