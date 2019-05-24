6680480eaf84b986b8f892448f613d6301f27c1c
Heku Watada <heku.watada@gmail.com>
2019-05-24 02:00:36 +0100

    Added example of writing as and reading from parquet.

---
+++ src/main/scala/org/netarrow/parquet/ParquetSparkApp.scala
@@ -0,0 +1,40 @@
+package org.netarrow.parquet
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
+import org.netarrow.app.SparkApp
+import org.netarrow.model.User
+
+//@see https://spark.apache.org/docs/latest/sql-data-sources-parquet.html
+
+object WriteAsParquetSparkApp extends App with SparkApp {
+  val appName = "WriteAsParquetSparkApp"
+  implicit val config = ConfigFactory.load()
+
+  run { ss: SparkSession =>
+    import ss.implicits._
+
+    val users = Seq(User("okame", 45), User("hyottoko", 19), User("okina", 37))
+    val ds: Dataset[User] = ss.sparkContext.parallelize(users).toDS()
+
+    ds.write.parquet("user.parquet")
+  }
+}
+
+object ReadFromParquetSparkApp extends App with SparkApp {
+  val appName = "ReadFromParquetSparkApp"
+  implicit val config = ConfigFactory.load()
+
+  run { ss: SparkSession =>
+    import ss.implicits._
+    val schema = Encoders.product[User].schema
+
+    val ds: Dataset[User] = ss.read
+      .schema(schema)
+      .parquet("user.parquet")
+      .as[User]
+
+    println(ds.first())
+  }
+}
+
